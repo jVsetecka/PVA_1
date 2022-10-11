28a9e5107b9a829cf2023426219980115348c39a
Všetečka Jakub <jakub.vsetecka@skola.ssps.cz>
2022-10-11 10:59:09 +0200

    hodina 4.10. - kalkulacka

---
+++ Kalkulacka/kalkulacka1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulacka1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1 = int.Parse(Console.ReadLine());
+            int cislo2 = int.Parse(Console.ReadLine());
+            string operace = Console.ReadLine();
+            double vysledek = 0;
+
+   
+            if (operace == "+")
+            {
+                vysledek = cislo1 + cislo2;
+            }
+            else if (operace == "-")
+            {
+                vysledek = cislo1 - cislo2;
+            }
+            else if (operace == "*")
+            {
+                vysledek = cislo1 * cislo2;
+            }
+            else if (operace == "/")
+            {
+                vysledek = cislo1 / cislo2;
+            }
+            else if (operace == "%")
+            {
+                vysledek = cislo1 % cislo2;
+            }
+            else
+            {
+                Console.WriteLine("Špatná operace");
+            }
+
+
+            Console.WriteLine("Výsledek: " + vysledek);
+            Console.ReadKey();
+        }
+    }
+}
