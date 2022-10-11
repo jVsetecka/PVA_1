5f0402e19f5094840acba024b1369487119ad2f1
Všetečka Jakub <jakub.vsetecka@skola.ssps.cz>
2022-10-11 10:47:01 +0200

    hodina 11.10. - pythagorova veta, min_max

---
+++ Pythagorova veta/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a, b, c;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+            if (a > c)
+            {
+                int tmp = c;
+                c = a;
+                a = tmp;
+            }
+            if (b > c)
+            {
+                int tmp = c;
+                c = b;
+                b = tmp;
+            }
+            
+            if (a * a + b * b == c * c)
+            {
+                Console.WriteLine("Je pravouhly\n");
+            }
+            else
+            {
+                Console.WriteLine("Neni pravouhly\n");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ min_max/min_max/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace min_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int pocet = int.Parse(Console.ReadLine());
+            int max = -1, min = Int32.MaxValue, soucet = 0;
+
+            Console.WriteLine();
+
+            for (int i = 0; i < pocet; i++)
+            {
+                Console.WriteLine("Číslo: " + (i+1));
+                int vstup = int.Parse(Console.ReadLine());
+
+                soucet += vstup;
+                if (vstup > max)
+                {
+                    max = vstup;
+                }
+                if (vstup < min)
+                {
+                    min = vstup;
+                }
+            }
+
+            Console.WriteLine("Min: " + min);
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Prumer: " + (soucet / pocet));
+
+            Console.ReadKey();
+          
+        }
+    }
+}
