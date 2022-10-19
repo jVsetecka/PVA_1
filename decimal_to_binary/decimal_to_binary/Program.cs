93365a910bc1629845bf663b80f9ca8d9390f5c5
Všetečka Jakub <jakub.vsetecka@skola.ssps.cz>
2022-10-19 10:58:44 +0200

    hodina 19.10. - binary_to_decimal

---
+++ decimal_to_binary/decimal_to_binary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace decimal_to_binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadejte decimalni cislo: ");
+            int dec = int.Parse(Console.ReadLine());
+            string bin = "";
+
+            do
+            {
+                int tmp = dec % 2;
+                bin = tmp + bin;
+
+                dec -= tmp;
+                dec = dec / 2;
+            } while (dec >= 1);
+
+            Console.WriteLine(bin);
+            Console.ReadKey();
+        }
+    }
+}
