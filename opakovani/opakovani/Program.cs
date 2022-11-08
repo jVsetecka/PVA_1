1e80b3f1da6fce822310fb140a2caa46972fab45
Všetečka Jakub <jakub.vsetecka@skola.ssps.cz>
2022-11-08 08:42:45 +0100

    hodina 8.11. - opakovani

---
+++ opakovani/opakovani/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opakovani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vstup = "0";
+            long cislo = 0;
+            bool spusteno = true;
+            while (spusteno)
+            {
+                vstup = Console.ReadLine();
+                if(vstup != "n")
+                {
+                    cislo = long.Parse(vstup);
+
+                    for (int i = 0; i < cislo; i++)
+                    {
+                        Console.WriteLine("Cyklus: " + i);
+                    }
+                }
+                else if(vstup == "n")
+                {
+                    spusteno = false;
+                }
+                else
+                {
+                    Console.WriteLine("spatny vstup");
+                }
+            }
+        }
+    }
+}
---
+++ opakovani/opakovani/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("opakovani")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("opakovani")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("4dd3833e-10e4-455c-8eb3-e7f32121553e")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
